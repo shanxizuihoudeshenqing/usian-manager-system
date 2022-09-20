9c0593e4e44c95807b882711a6f629d77def2daa
chen <53075695@qq.com>
2022-09-20 21:28:18 +0800

    feat(custom): 配置了标题，icon图标，serve

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,18 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
+  devServer: {
+    // 端口号
+    port: 9999,
+    // 主机名
+    host: "localhost",
+    // 是否自动打开浏览器
+    open: true,
+    // 是否开启https
+    https: false,
+    // 深入学习webpack --- 国庆作业
+  },
+
+  // 关闭eslint
+  lintOnSave:false
 });
